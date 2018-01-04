321b00c7c12a13978aacca7b8e21f4b708c69ef1
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2018-01-04 23:57:09 +0800

    rework disabled state of the update button

--- src/components/ProductEditPane.vue
+++ src/components/ProductEditPane.vue
@@ -1,52 +1,40 @@
 <template>
   <article id="product-edit-pane">
-    <b-field
-      grouped
-      position="is-right"
-    >
+    <b-field grouped
+             position="is-right">
       <b-field expanded>
-        <b-input
-          placeholder="3M 編號"
-          v-model.trim.lazy="convFactorId"
-        />
+        <b-input placeholder="3M 編號"
+                 v-model.trim.lazy="convFactorId" />
       </b-field>
       <b-field expanded>
-        <b-input
-          type="number"
-          step="0.1"
-          min="0"
-          placeholder="轉換率"
-          v-model.number.lazy="convFactor"
-        />
+        <b-input type="number"
+                 step="0.1"
+                 min="0"
+                 placeholder="轉換率"
+                 v-model.number.lazy="convFactor" />
       </b-field>
       <b-field>
         <p class="control">
-          <button
-            class="button is-info"
-            :disabled="isPrestine"
-            @click="$emit('edit', { id: id, conversionFactorId: convFactorId, conversionFactor: convFactor})"
-          >
+          <button class="button is-info"
+                  :disabled="isPrestine || hasEmptyValue"
+                  @click="$emit('edit', { id: id, conversionFactorId: convFactorId, conversionFactor: convFactor})">
             更新
           </button>
         </p>
       </b-field>
       <b-field>
         <p class="control">
-          <button
-            class="button is-success"
-            :disabled="!conversionFactorId"
-            @click="$emit('clear', id)"
-          >
+          <button class="button is-success"
+                  :disabled="!conversionFactorId"
+                  @click="$emit('clear', id)">
             重置
           </button>
         </p>
       </b-field>
       <b-field>
         <p class="control">
-          <button
-            class="button is-danger"
-            @click="$emit('close', id)"
-          >
+          <button class="button is-danger"
+                  @click="$emit('close', id)">
             關閉
           </button>
         </p>
@@ -56,10 +44,8 @@
       <hr>
       <h6 class="subtitle is-6">以下產品資料記錄與新資料產生重疊，請確認無誤再點選'更新'</h6>
       <ul>
-        <li
-          v-for="(product, index) in lookupResults"
-          :key="index"
-        >
+        <li v-for="(product, index) in lookupResults"
+            :key="index">
           {{ index + 1 }}.【{{ product.id }}】{{ product.name }} 【SAP 編號:
           <strong>{{ product.conversionFactorId }}</strong> 轉換率:
           <strong>{{ product.conversionFactor }}</strong>】
@@ -90,8 +76,8 @@ export default {
   },
   data () {
     return {
-      convFactorId: null,
-      convFactor: null,
+      convFactorId: '',
+      convFactor: '',
       lookupResults: [],
     }
   },
@@ -102,15 +88,32 @@ export default {
         (this.convFactor === this.conversionFactor)
       )
     },
+    hasEmptyValue () {
+      return (
+        (this.convFactorId === null) ||
+        (this.convFactor === null)
+      )
+    },
   },
   watch: {
     convFactorId (convFactorId) {
-      return this.check({
-        id: this.id,
-        conversionFactorId: convFactorId,
-      }).then(results => {
-        this.lookupResults = results
-      })
+      if (convFactorId === '') {
+        this.convFactorId = null
+      } else {
+        return this.check({
+          id: this.id,
+          conversionFactorId: convFactorId,
+        }).then(results => {
+          this.lookupResults = results
+        })
+      }
+    },
+    convFactor (convFactor) {
+      if (convFactor === '') {
+        this.convFactor = null
+      } else {
+        this.convFactor = convFactor.toFloat()
+      }
     },
   },
   mounted () {
