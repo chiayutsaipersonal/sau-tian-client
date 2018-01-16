42ba48f101f0f8610fc5e7975bf19ea63c5c270a
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2018-01-16 22:22:10 +0800

    insignificant change

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import Buefy from 'buefy'
 import 'buefy/lib/buefy.css'
+
 import App from './App'
 import router from './router'
-
 import store from './store'
 
-Vue.use(Vuex)
 Vue.use(Buefy, { defaultIconPack: 'fa' })
+Vue.use(Vuex)
 
 Vue.config.productionTip = true
 
