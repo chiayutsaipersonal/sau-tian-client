de1630880db6ebde84953a5e0c1599c65a9d5428
chiayutsaipersonal <chiayu.tsai.personal@gmail.com>
2018-01-22 23:41:59 +0800

    view
    update instructions page to reflect it's not done

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,18 +1,8 @@
 <template>
   <div id="home-page">
     <h3 class="title is-3">系統操作說明</h3>
-    <p>網頁開啟後，程式將自動檢查使否有上一次存留下來的操作資料</p>
-    <p>asfaksjf;askjdfa;sjf;asd</p>
-    <p>asfaksjf;askjdfa;sjf;asd</p>
-    <p>asfaksjf;askjdfa;sjf;asd</p>
-    <p>asfaksjf;askjdfa;sjf;asd</p>
-    <p>asfaksjf;askjdfa;sjf;asd</p>
-    <p>asfaksjf;askjdfa;sjf;asd</p>
-    <p>asfaksjf;askjdfa;sjf;asd</p>
-    <p>asfaksjf;askjdfa;sjf;asd</p>
-    <p>asfaksjf;askjdfa;sjf;asd</p>
-    <p>asfaksjf;askjdfa;sjf;asd</p>
-    <p>asfaksjf;askjdfa;sjf;asd</p>
+    <hr>
+    <p>未完成</p>
   </div>
 </template>
 
